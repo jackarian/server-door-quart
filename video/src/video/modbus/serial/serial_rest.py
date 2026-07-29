b0a9ecd5cace6e7e173693eda530342cc1eb9ccb
jackarian <ariannagiacomo@gmail.com>
2026-07-29 21:40:29 +0200

    feat: update to 12 for manage serial access

--- video/src/video/modbus/serial/serial_rest.py
+++ video/src/video/modbus/serial/serial_rest.py
@@ -1,11 +1,17 @@
 from video.modbus.model.serial_model import ModbusPayload
 from video.modbus.serial.serial_controller import ModbusSerialClientService
-
+from pymodbus import FramerType
+from pymodbus.client import ModbusSerialClient
 
 
 class ModbusSerialRestController:
     def __init__(self) -> None:
-        self.service = ModbusSerialClientService(port='/dev/ttyUSB0',baudrate=9600)
+        self.service = ModbusSerialClient(port='/dev/ttyUSB1', framer = FramerType.RTU,
+                 baudrate= 9600, bytesize= 8, parity= "N",
+                 stopbits = 1, handle_local_echo = False,
+                 name= "comm", reconnect_delay = 0.1,
+                 reconnect_delay_max= 300,
+                 timeout= 3, retries = 3)
 
     def execute(self,command:ModbusPayload):
         self.service.connect()
